2677a909776cf14fc092c8592c06dcfbb86fc9bd
Chubbod <john_wilson_chua@dlsu.edu.ph>
2022-11-08 17:06:27 +0800

    Art, PlayerMovement, Boundaries
    (1) Added Sprites, and displayed Background.
    (2) Provided the Horizontal Movement
    (3) Limit the Horizontal Movement to Screen-size

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float maxSpeed = 3f;                //speed of movement
+    float shipBoundaries = 0.6f;        //precaution for when the ship touches the edge of the screen
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Horizontal Movement
+        Vector3 pos = transform.position;
+        pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;  
+
+        //Set Boundaries of the Player
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrthographic = Camera.main.orthographicSize * screenRatio;
+
+        if(pos.x + shipBoundaries > widthOrthographic) {
+            pos.x = widthOrthographic - shipBoundaries;
+        }
+
+        if(pos.x - shipBoundaries < -widthOrthographic) {
+            pos.x = -widthOrthographic + shipBoundaries;
+        }
+
+        //Update the ship position
+        transform.position = pos;
+    }
+}
