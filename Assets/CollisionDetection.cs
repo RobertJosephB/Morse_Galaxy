0f599a2f696fd058ba57c2f46c4c2369bafa8559
Chubbod <john_wilson_chua@dlsu.edu.ph>
2022-11-09 13:53:47 +0800

    Background Change, Bullet-Change, Collision takes effects
    (1) Separated the Background with the Earth Model.
    (2) A Bullet fired should be on 1 target? or the bullet fired should be separated?
    (3) On Collision, Earth dies after 5 hits, Enemy dies after 1, and Player dies after 3.

--- Assets/CollisionDetection.cs
+++ Assets/CollisionDetection.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
+    public int health = 1;          //Health in Changeable through Unity
+    public float invulPeriod = 0;   //Default at 0 for all, player ship protect on 2nd instant damage
+    float invulTimer = 0;
+    int correctLayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        correctLayer = gameObject.layer;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Hit! " + gameObject.name);
+
+        health--;
+        invulTimer = invulPeriod;
+        gameObject.layer = 8;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        invulTimer -= Time.deltaTime;
+        if(invulTimer <= 0) {
+            gameObject.layer = correctLayer;
+        }
+
+        if(health <= 0) {
+            Destroyed();
+        }
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.name == "Ufos")
-        {
-            Debug.Log("Collision Detected!");
-        }
+    void Destroyed() {
+        Destroy(gameObject);
     }
 }
